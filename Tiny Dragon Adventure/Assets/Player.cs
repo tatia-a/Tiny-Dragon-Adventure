6c64ba2ba8c5acb150cb537ebcaae6fd028fd51e
tatia-a <108894837+tatia-a@users.noreply.github.com>
2022-11-22 22:53:30 +0300

    Улучшение сцены и движения персонажа

---
+++ Tiny Dragon Adventure/Assets/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        print(horizontalThrow);
+    }
+}
